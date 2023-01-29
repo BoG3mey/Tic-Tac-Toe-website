130c322c5f55b2aedf900440d3bbf54018663e7e
abik <nikita161787@gmail.com>
2023-01-29 22:22:27 +0100

    2

--- main/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Task(models.Model):
-    title = models.CharField('Название', max_length=50)
-    task = models.TextField('Описание')
-    def _str_(self):
-        return self.title
